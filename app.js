2e357642f1f8a8a3346dc9dd6232369e2ecc7f2b
Noushad Mehdi <engr.noushadmehdi@gmail.com>
2020-06-15 22:39:06 +0500

    initial commit

---
+++ app.js
@@ -0,0 +1,184 @@
+//---------------------------------------------------Chapter no.1-----------------------------------------------------------
+//Task 1
+//alert("Welcome")
+//Task 2
+//alert("JavaScript Alert! \n\nError! Please enter a valid password")
+//Task 3
+//alert("JavaScript Alert! \n\nWelcome to JS Land \nHappy Coding")  
+// Task 4
+//alert("JavaScript Alert! \nWelcome to JS Land.....")
+//alert("JavaScript Alert! \nHappy Coding.....")
+// Task 5
+//alert("JavaScript Alert! \nHello I can run JS from my Web browsers console.")
+
+//---------------------------------------------------Chapter No.2-------------------------------------------------------------
+//Task 1
+//var username
+
+//Task 2
+//var myname="Noushad Mehdi"
+
+//Task 3
+//var message="Hello World"
+//alert(message)
+
+//Task 4
+//var name="John Doe"
+//var age="15 years Old"
+//var qualification="Certified Mobile Application Development"
+//alert(name)
+//alert(age)
+//alert(qualification)
+
+//Task 5
+//var a="PIZZA \nPIZZ \nPIZ \nPI \nP"
+//alert(a)
+
+//Task 6
+//var email="engr.noushadmehdi@gmail.com"
+//alert("My email address is " + email)
+
+//Task 7
+//var book="A smarter way to learn Javascript"
+//alert("I am trying to learn from book " + book)
+
+//Task 8
+//document.write("Yah! I can write the HTML content through Javascript")
+
+//Task 9
+//var a = "----------Hello------------"
+//alert(a)
+//document.write(a)
+
+//-----------------------------------------------------Chapter No. 3--------------------------------------------------------------------------
+//Task 1
+//var age = 28
+//alert("I am " + age + " years old")
+
+//Task 2
+//var a = 15
+//alert("You have visited this site " + a + " times")
+
+//Task 3
+//birthYear = "1992"
+//document.write("My birth year is " + birthYear "\nData type of my declared variable is Number")
+
+//Task 4
+//visitorsName = "John Doe"
+//productTitle = "T-shirts"
+//Quantity = 5
+//document.write("visitorsName + "\nordered" + Quantity + " " + productTitle + " on XYZ Clothing Store")
+
+
+//----------------------------------------------------Chapter No.4---------------------------------------------------------------------
+//Task 1
+// var Variable1="Var 1", Variable2="Var 2", Variable3 = "Var 3";
+
+//Task 2
+//5 illegal Variable names
+//var variable 1 = 1.25
+//var P2_a%88$
+//var my variable
+
+//5 legal Variable names
+//var π = Math.PI;
+//var myName
+//var var1
+//var $123
+//var var_1
+
+//Task 3
+//document.write("<h1>Rules for naming JS Variables</h1>")
+//document.write("<br>Variable names can only contain numbers,$ and _. For example$my1st_variable")
+//document.write("<br>Variable must begin with the letters, $ or _. For example $name,_name, or name")
+//document.write("<br>Variable names are case sensitive")
+//document.write("<br>Variable names should not be JS Keywords")
+
+//-----------------------------------------------------------------Chapter 5----------------------------------------------------------------
+
+//Task 1
+//var a = 5;
+//var b = 3;
+//var c = a + b;
+//document.write("The sum of " + a + "\nand\n" + b + "\nis\n" + c);
+
+//Task 2
+//var a = 5;
+//var b = 3;
+//var c = a / b;
+//document.write("The division of " + a + "\nand\n" + b + "\nis\n" + c);
+
+//var a = 5;
+//var b = 3;
+//var c = a % b;
+//document.write("The remainder of " + a + "\nand\n" + b + "\nis\n" + c);
+
+//var a = 5;
+//var b = 3;
+//var c = a * b;
+//document.write("The mutiplication of " + a + "\nand\n" + b + "\nis\n" + c);
+
+//Task 4
+//var ticketPrice = 600
+//var a = 5 * 600;
+//document.write("The cost to buy 5 tickets to a movie is" + a "PKR"")
+
+//Task 7
+// var Item1 = 650;
+// var Item2 = 100;
+// var Item1qty = 3;
+// var Item2qty = 7;
+// var SC = 100;
+// var TC = (650*3) + (100*7) + SC;
+//document.write("Total cost of your order is" + TC);
+
+//Task 8
+//var TM = 850;
+// var MO = 746;
+// var perc = (MO/TM) * 100;
+//document.write("<h1>Marksheet result<\h1>")
+//document.write("Total Marks" + "=" + TM);
+//document.write("Marks Obtained" + "=" + MO);
+//document.write("Percentage" + "=" + perc);
+
+//Task 10
+//var a = 5;
+// var b = ((a + 5)*10) / 2;
+
+//Task 11
+//var CY = 2020;
+// var BY = 1992;
+// var age = CY - BY;
+//document.write("Current year:" + CY);
+//document.write("Birth Year:" + BY);
+//document.write("Your age is " + age);
+
+//Task 12
+// var radius = 5;
+// var circumference = 2 * 3.14 * 5;
+// var area = 3.14 * 5 * 5;
+//document.write("Radius ofa circle:" + radius);
+//document.write("Circumference of circle: " + circumference);
+//document.write("Area of circle: " + area);
+
+//-------------------------------------------------------------------Chapter 6-9--------------------------------------------------------------
+
+//Task 1
+// var a = 10
+//document.write("The value of a is 10");
+// a = ++a;
+// document.write("The value of ++a is" + a);
+// document.write("Now the value of a is: " + a);
+// a= a++;
+// document.write("The value of a++ is " + a);
+// document.write("Now the value of a is " + a);
+
+//Task 2
+// var a = 2;
+// var b = 1;
+// var result = --a - --b + ++b + b--;
+// document.write("Result is: " + result);
+
+//Task 3
+//var name = window.prompt("Enter your name: ");
+//alert("Welcome " + name);
